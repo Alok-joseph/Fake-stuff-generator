94add33e5cfe0928b67be08938599ae1633a6923
Alok-JOSEPH <98444143+Alok-joseph@users.noreply.github.com>
2022-02-12 11:56:21 +0530

    Create fake_creditcard.py

---
+++ fake_creditcard.py
@@ -0,0 +1,14 @@
+from faker import *
+
+faker = Faker()
+print("credit card")
+answer = str(input())
+credit_card = "c"
+print("how many")
+m = int(input())
+
+    for x in range(m):
+      print(faker.credit_card_full)
+
+
+
